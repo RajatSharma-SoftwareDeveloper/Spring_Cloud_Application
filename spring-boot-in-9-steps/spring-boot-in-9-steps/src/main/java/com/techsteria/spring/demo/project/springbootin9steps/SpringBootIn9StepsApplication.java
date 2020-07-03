fc6d650910e78f1deb833966c800dc91191eab90
Rajat Sharma <rajat009sharma@gmail.com>
2020-07-04 02:35:32 +0530

    Spring_Boot_Application

---
+++ spring-boot-in-9-steps/spring-boot-in-9-steps/src/main/java/com/techsteria/spring/demo/project/springbootin9steps/SpringBootIn9StepsApplication.java
@@ -0,0 +1,15 @@
+package com.techsteria.spring.demo.project.springbootin9steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIn9StepsApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIn9StepsApplication.class, args);
+		System.out.println("Hello World application ....");
+	}
+
+}
---
+++ spring-boot-in-9-steps/spring-boot-in-9-steps/src/test/java/com/techsteria/spring/demo/project/springbootin9steps/SpringBootIn9StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsteria.spring.demo.project.springbootin9steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIn9StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
